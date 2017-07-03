23c9d23154dad208b9daa337a5931041a0355254
Matthew Goslett <matthew@goslett.co.za>
2017-07-03 10:28:16 +0200

    change InvalidArgumentException to extend \InvalidArgumentException

--- src/InvalidArgumentException.php
+++ src/InvalidArgumentException.php
@@ -2,9 +2,8 @@
 
 namespace Superbalist\Laravel4PSR6CacheBridge;
 
-use Exception;
 use Psr\Cache\InvalidArgumentException as InvalidArgumentExceptionInterface;
 
-class InvalidArgumentException extends Exception implements InvalidArgumentExceptionInterface
+class InvalidArgumentException extends \InvalidArgumentException implements InvalidArgumentExceptionInterface
 {
 }
